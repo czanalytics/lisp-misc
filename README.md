6872ae4ac3fced5c536f1fb8ee6287728d66da9a
aatos <aatos.heikkinen@czanalytics.com>
2015-02-08 12:15:01 +0200

    1st Common Lisp notes

---
+++ README.md
@@ -0,0 +1,4 @@
+*** lisp-misc
+
+Misc. Lisp scripts and notes excludling Clojure, which
+is covered in clj-* projects.
---
+++ lang.lisp
@@ -0,0 +1,19 @@
+;;;; Notes on basic Common Lisp features
+;;;; C.Barski: Land of Lisp (2011)
+;;;;
+;;;; SBCL  
+
+
+;;; REPL 
+(+ 1 (* 2 3))
+
+;;; 
+(defparameter *foo* 1)
+
+;;; functions
+(defun foo ()
+  ;; dummy
+ () ;
+ (+ 2 3))
+
+(foo)
